e5e43dd97b4dd4e5a157e21872ccd617544b8ecf
Sean Agustine Esparagoza <agustinelumandong@gmail.com>
2026-03-01 17:38:33 +0800

    feat: expand first-run choices with new optional applications

--- install/first-run-choices.sh
+++ install/first-run-choices.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Only ask for default desktop app choices when running Gnome
-if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]]; then
-  OPTIONAL_APPS=("1password" "Spotify" "Zoom" "Dropbox")
-  DEFAULT_OPTIONAL_APPS='1password,Spotify,Zoom'
-  export OMAKUB_FIRST_RUN_OPTIONAL_APPS=$(gum choose "${OPTIONAL_APPS[@]}" --no-limit --selected $DEFAULT_OPTIONAL_APPS --height 7 --header "Select optional apps" | tr ' ' '-')
+# Only ask for default desktop app choices when running Gnome or COSMIC
+if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]] || [[ "$XDG_CURRENT_DESKTOP" == *"COSMIC"* ]]; then
+  OPTIONAL_APPS=("1password" "Spotify" "Zoom" "Dropbox" "Firefox" "Postman" "Peek" "Font-Manager" "Bruno" "Chromium" "Edge" "Terraform" "Kubectl" "Helm" "AWS-CLI" "Azure-CLI" "Podman" "Azure-Storage-Explorer" "Htop" "BleachBit" "Wireshark" "USBGuard" "JetBrains-Toolbox" "Eclipse" "Dotnet" "Delve" "Slack" "Teams" "Zed" "LazyNPM" "LazySQL" "Beekeeper-Studio" "Ngrok" "YouTube-Music" "Sober" "Vinegar")
+  DEFAULT_OPTIONAL_APPS='1password,Spotify,Zoom,Firefox,Terraform,Kubectl'
+  export OMAKUB_FIRST_RUN_OPTIONAL_APPS=$(gum choose "${OPTIONAL_APPS[@]}" --no-limit --selected $DEFAULT_OPTIONAL_APPS --height 36 --header "Select optional apps" | tr ' ' '-')
 fi
 
 AVAILABLE_LANGUAGES=("Ruby on Rails" "Node.js" "Go" "PHP" "Python" "Elixir" "Rust" "Java")
-SELECTED_LANGUAGES="Ruby on Rails","Node.js"
+SELECTED_LANGUAGES="Python","Node.js", "PHP"
 export OMAKUB_FIRST_RUN_LANGUAGES=$(gum choose "${AVAILABLE_LANGUAGES[@]}" --no-limit --selected "$SELECTED_LANGUAGES" --height 10 --header "Select programming languages")
 
 AVAILABLE_DBS=("MySQL" "Redis" "PostgreSQL")
-SELECTED_DBS="MySQL,Redis"
+SELECTED_DBS="MySQL,Redis,PostgreSQL"
 export OMAKUB_FIRST_RUN_DBS=$(gum choose "${AVAILABLE_DBS[@]}" --no-limit --selected "$SELECTED_DBS" --height 5 --header "Select databases (runs in Docker)")
